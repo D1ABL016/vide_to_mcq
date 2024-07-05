6386a2d203267cac4a6a01932f792481d467f166
Lakshay Jain <133592963+D1ABL016@users.noreply.github.com>
2024-07-05 14:09:47 +0530

    Add files via upload

---
+++ video-to-mcq/main.py
@@ -0,0 +1,22 @@
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users may need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+
+aai.settings.api_key = "aded0399c8dc45e5b605a8856af99fd6"
+
+# URL of the file to transcribe
+FILE_URL = "./audio1.mp3"
+
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe(FILE_URL)
+
+if transcript.status == aai.TranscriptStatus.error:
+    print(transcript.error)
+else:
+    print(transcript.text)
